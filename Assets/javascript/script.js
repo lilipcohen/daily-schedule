4893a20b4037a8f7f783c17a9075a41ccc3d75d9
Lili Cohen <lili.cohen.tv@gmail.com>
2020-11-05 14:10:14 -0800

    adding object array

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,71 +1,79 @@
 $(document).ready(function () {
-        //Prints current date at the top
-        $("#currentDay").text(moment().format("dddd, MMM Do"));
+    var userInput = [
+        { time: 7, input: "" },
+        { time: 8, input: "" },
+        { time: 9, input: "Wake up" },
+        { time: 10, input: "" },
+        { time: 11, input: "Go to work" },
+        { time: 12, input: "" },
+        { time: 13, input: "" },
+        { time: 14, input: "" },
+        { time: 15, input: "" },
+        { time: 16, input: "" },
+        { time: 17, input: "" }
+    ]
 
-        var userInput = "";
-        var hours = ["9","10","11","12","1","2","3","4","5"];
-        var currentHour = moment().hours();
-      
-        //For loop for hours to print all at once 
-        //divHour not defined currently
-        for(i = 0; i < hours.length; i++){
-            $(".hour").text(hours[i]);
+    //var hours = ["9","10","11","12","1","2","3","4","5"];
+    var currentHour = moment().hours();
+    console.log(currentHour);
+    
+    //Prints current date at the top
+    $("#currentDay").text(moment().format("dddd, MMM Do"));
+
+    //For loop for hours to print all at once 
+    //divHour not defined currently
+    /*for(i = 0; i < hours.length; i++){
+        $(".hour").text(hours[i]);
+    }
+    */
+    //Timeblocks will repeat 9 times for 9am-5pm
+    for (var i = 0; i < 9; i++){
+        var container = $(".container");
+        var timeBlock = $("<div>").addClass("time-block");
+        var divRow = $("<div>").addClass("row");
+        var divHour = $("<div>").addClass("hour col-lg-2");
+        var textInput = $("<textarea>").addClass("col-lg-8");
+        var saveButton = $("<button>").addClass("saveBtn col-lg-2");
+        var descrip = $("<div>").addClass("description");
+        $(container).append(timeBlock);
+        $(timeBlock).append(divRow);
+        $(divRow).append(divHour);
+        //$(divHour).text(hours[i]);
+        $(divRow).append(textInput);
+        $(divRow).append(saveButton);
+        $(timeBlock).append(descrip);
+    }
+
+
+    //Set times to whatever current time is
+    function compareTime(){
+        //use moment.js to set time equal to value of class hour 9AM-5PM
+        parseInt(hours)
+        currentHour === hours;
+        //if hour = currentHour then hour class is present
+        if (hours < currentHour) {
+            textInput.addClass("past");
         }
-
-        //If need to create timeblocks through script then:
-        //ask if this is correct?
-        //Do columns work here??
-        for (var i = 0; i < 9; i++){
-            function blockSetUp(){
-                var container = $(".container");
-                var timeBlock = $("<div>").addClass("time-block");
-                var divRow = $("<div>").addClass("row");
-                var divHour = $("<div>").addClass("hour col-lg-2");
-                var textInput = $("<textarea>").addClass("past col-lg-8");
-                var saveButton = $("<button>").addClass("saveBtn col-lg-2");
-                var descrip = $("<div>").addClass("description");
-                $(container).append(timeBlock);
-                $(timeBlock).append(divRow);
-                $(divRow).append(divHour);
-                $(divHour).text(hours[i]);
-                $(divRow).append(textInput);
-                $(divRow).append(saveButton);
-                $(timeBlock).append(descrip);
-            }
-            blockSetUp();
+        //else if hour < currentHour then hour class is past
+        else if (hours === currentHour) {
+            textInput.addClass("present");
         }
-
-
-        //Set times to whatever current time is
-        function compareTime(){
-          //use moment.js to set time equal to value of class hour 9AM-5PM
-            currentHour === hours;
-          //if hour = currentHour then hour class is present
-            if (hours < currentHour) {
-                
-            }
-          //else if hour < currentHour then hour class is past
-
-          //else if hour > currentHour then hour class is future
-
-          //Make sure to set currentHour to 9-5?
+        //else if hour > currentHour then hour class is future
+        else if (hours > currentHour) {
+            textInput.addClass("future");
         }
+        //Make sure to set currentHour to 9-5?
+    }
+    compareTime();
 
-        //Create an onclick function for the timeblocks that?
-        //Is that necessary if there's a textarea?
-        //redefine the userInput for each timeblock
-        $(".time-block").on("click", function userText(){
+    //Create save on click save button to take the textarea
+    //and save it to local storage for that specific timeblock
+     //Get local save
+    localStorage.getItem(userInput, '');
 
-        });
+    $(".saveBtn").on("click", function saveNew(){
 
-        //Create save on click save button to take the userInput
-        //and save it to local storage for that specific timeblock
-        $(".saveBtn").on("click", function saveNew(){
-
-        //Set local save
-        localStorage.setItem("", );
-
-        //Get local save
-        localStorage.setItem("", );
-        });
-      });
+    //Set local save
+    localStorage.setItem(userInput, '');
+    });
+});
